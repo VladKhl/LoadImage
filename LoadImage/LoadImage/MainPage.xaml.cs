960540215bb5759766bee5267d387dcc5d1fdbb7
Владислав Хлыбов <vavan8765@gmail.com>
2022-02-11 10:57:37 +0300

    Добавьте файлы проекта.

---
+++ LoadImage/LoadImage/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using LoadImage.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace LoadImage
+{
+    public partial class MainPage : ContentPage
+    {
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        string impath;
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+        async void GetPhotoAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                var photo = await MediaPicker.PickPhotoAsync();
+                impath = photo.FullPath;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Сообщение об ошибке", ex.Message, "OK");
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            UpdateList();
+        }
+
+        async void TakePhotoAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions
+                {
+                    Title = $"xamarin.{DateTime.Now.ToString("dd.MM.yyyy_hh.mm.ss")}.png"
+                });
+                var newFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), photo.FileName);
+                using (var stream = await photo.OpenReadAsync())
+                using (var newStream = File.OpenWrite(newFile))
+                    await stream.CopyToAsync(newStream);
+
+                Debug.WriteLine($"Путь фото {photo.FullPath}");
+                impath = photo.FullPath;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Сообщение об ошибке", ex.Message, "OK");
+            }
+        }
+
+        public void UpdateList()
+        {
+            imageList.ItemsSource = null;
+            imageList.ItemsSource = App.Db.GetObjects();
+        }
+
+        private void add_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                App.db.SaveItem(new ImageModel(name.Text, impath));
+                DisplayAlert("", "Обьект успешно добавлен", "Ok");
+                UpdateList();
+            }
+            catch
+            {
+                DisplayAlert("", "Не удалось добавить обьект", "Ok");
+            }
+        }
+
+        private void imageList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Navigation.PushAsync(new ObPage((ImageModel)e.Item));
+        }
+    }
+}
---
+++ LoadImage/LoadImage/ObPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using LoadImage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoadImage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ObPage : ContentPage
+    {
+        readonly ImageModel im;
+        public ObPage(ImageModel im)
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            this.im = im;
+            name.Text = im.Name;
+            img.Source = im.Image;
+        }
+    }
+}
